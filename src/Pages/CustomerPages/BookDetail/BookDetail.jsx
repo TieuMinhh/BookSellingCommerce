d8b55e501e0b74d568dce4d7d86b269658423924
Xiao Ming <minbao4869@gmail.com>
2023-10-04 08:45:56 +0700

    Merge khong duoc cay qua

--- src/Pages/CustomerPages/BookDetail/BookDetail.jsx
+++ src/Pages/CustomerPages/BookDetail/BookDetail.jsx
@@ -102,7 +102,7 @@ export default function BookDetail() {
               <p
                 style={{
                   fontSize: "18px",
-                  marginTop: "16px",
+                  // marginTop: "16px",
                 }}
               >
                 {number}
