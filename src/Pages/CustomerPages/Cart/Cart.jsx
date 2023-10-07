e75b8bcfe23ffa44d474142a28baa65b8ab0634e
Tuan <quoctuanit87@gmail.com>
2023-10-07 11:40:03 +0700

    fix

--- src/Pages/CustomerPages/Cart/Cart.jsx
+++ src/Pages/CustomerPages/Cart/Cart.jsx
@@ -122,7 +122,7 @@ export default function Cart(props) {
         <div className="container">
             <div class="cart-title">
                 <h2 class="">GIỎ HÀNG</h2>
-                <span class="font-16">({list.length} sản phẩm)</span>
+                <span class="font-16">({list?.length} sản phẩm)</span>
             </div>
             <div className="cart">
                 <div className="products">
