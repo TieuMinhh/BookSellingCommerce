69d454f814b97c79b2db1bbb7857d8f76d34ad5d
Xiao Ming <minbao4869@gmail.com>
2023-11-07 21:57:47 +0700

    Update API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-    // baseURL: 'https://apibookshop.up.railway.app/api',
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'https://apibookshop.up.railway.app/api',
+    // baseURL: 'http://localhost:8081/api',
 });
