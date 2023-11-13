312384363e90af09775b108a5e6e8dafb311fe52
Xiao Ming <minbao4869@gmail.com>
2023-11-14 00:16:35 +0700

    Update API Link

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
-    // PUBLIC_IMAGE_URL: 'https://apibookshop.up.railway.app/image/',
-    PUBLIC_IMAGE_URL: 'http://localhost:8081/image/',
+    PUBLIC_IMAGE_URL: 'https://apibookshop.up.railway.app/image/',
+    // PUBLIC_IMAGE_URL: 'http://localhost:8081/image/',
 };
 
 export default config;
